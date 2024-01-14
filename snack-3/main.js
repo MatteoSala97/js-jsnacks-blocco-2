1170aa8d5861bbc63070c725bd91d214d13025b1
MatteoSala97 <matteo111997@gmail.com>
2024-01-14 20:07:12 +0100

    snack 3 - uncompleted

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,10 +1,7 @@
 const arrayFirst = ["Element 1", "Element 2", "Element 3", "Element 4", "Element 5", "Element 6", "Element 7", "Element 8", "Element 9",
  "Element 10"]
 
- const arraySecond = ["Element Alpha", "Element Beta", "Element Gamma", "Element Delta", "Element Epsilon", "Element Zeta", "Element Eta", "Element Theta", "Element Iota", "Element Kappa", "Element Lambda", "Element Mu"]
+const arraySecond = ["Element Alpha", "Element Beta", "Element Gamma", "Element Delta", "Element Epsilon", "Element Zeta", "Element Eta", "Element Theta", "Element Iota", "Element Kappa", "Element Lambda", "Element Mu"]
 
 console.log("Array First:", arrayFirst)
 console.log("Array Second:", arraySecond)
-
-
-
