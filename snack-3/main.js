02937ccdd1eec2b8947482f911bf819e93feb68d
MatteoSala97 <matteo111997@gmail.com>
2024-01-14 23:20:30 +0100

    snack 3 fixed

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,14 +1,19 @@
-let arrayFirst = ["Element 1", "Element 2", "Element 3", "Element 4", "Element 5", "Element 6", "Element 7", "Element 8", "Element 9",
- "Element 10"]
+let arrayFirst = ["Element 1", "Element 2", "Element 3", "Element 4", "Element 5", "Element 6", "Element 7", "Element 8", "Element 9", "Element 10"]  //10elements
 
-let arraySecond = ["Element Alpha", "Element Beta", "Element Gamma", "Element Delta", "Element Epsilon", "Element Zeta", "Element Eta", "Element Theta", "Element Iota", "Element Kappa", "Element Lambda", "Element Mu"]
+let arraySecond = ["Element Alpha", "Element Beta", "Element Gamma", "Element Delta", "Element Epsilon", "Element Zeta", "Element Eta", "Element Theta", "Element Iota", "Element Kappa", "Element Lambda", "Element Mu"] //12elements
 
-console.log("Array First:", arrayFirst)
-console.log("Array Second:", arraySecond)
+// console.log("Array First:", arrayFirst)
+// console.log("Array Second:", arraySecond)
 
-// i dont know how to do it
+while (arrayFirst.length < arraySecond.length){
 
-if (arrayFirst.length < arraySecond.length){
     //push a random element of array second into array first
-    let randomElement = Math.floor(Math.random() * arrayFirst.length)
+    let randomElement = Math.floor(Math.random() * arraySecond.length)
+    console.log(randomElement)
+
+    arrayFirst.push(arraySecond[randomElement])
+    
+    console.log("Array First:", arrayFirst)
+    console.log("Array Second:", arraySecond)
+
 } 
