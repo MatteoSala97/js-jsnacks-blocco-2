92bea40b0ee99f746dedaa4a9de687ae5b1e5cea
MatteoSala97 <matteo111997@gmail.com>
2024-01-14 20:14:59 +0100

    snack 3 - attempted one more time, unsuccessfully

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,7 +1,14 @@
-const arrayFirst = ["Element 1", "Element 2", "Element 3", "Element 4", "Element 5", "Element 6", "Element 7", "Element 8", "Element 9",
+let arrayFirst = ["Element 1", "Element 2", "Element 3", "Element 4", "Element 5", "Element 6", "Element 7", "Element 8", "Element 9",
  "Element 10"]
 
-const arraySecond = ["Element Alpha", "Element Beta", "Element Gamma", "Element Delta", "Element Epsilon", "Element Zeta", "Element Eta", "Element Theta", "Element Iota", "Element Kappa", "Element Lambda", "Element Mu"]
+let arraySecond = ["Element Alpha", "Element Beta", "Element Gamma", "Element Delta", "Element Epsilon", "Element Zeta", "Element Eta", "Element Theta", "Element Iota", "Element Kappa", "Element Lambda", "Element Mu"]
 
 console.log("Array First:", arrayFirst)
 console.log("Array Second:", arraySecond)
+
+// i dont know how to do it
+
+if (arrayFirst.length < arraySecond.length){
+    //push a random element of array second into array first
+    let randomElement = Math.floor(Math.random() * arrayFirst.length)
+} 
